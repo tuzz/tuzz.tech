ac6c44a05ec5f70072b83d65947aedb735144781
Chris Patuzzo <chris@patuzzo.co.uk>
2019-09-29 17:47:38 +0100

    Set the viewport

--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import NavBar from "../nav_bar";
 import Footer from "../footer";
 import css from "./styles.scss";
@@ -5,6 +6,10 @@ import css from "./styles.scss";
 import "./reset.scss";
 
 const Layout = ({ children }) => <>
+  <Head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  </Head>
+
   <div className={css.layout}>
     <NavBar />
 
