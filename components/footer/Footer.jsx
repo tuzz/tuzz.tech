11d59f53081a47708427893655b0d0914fd06b4b
Chris Patuzzo <chris@patuzzo.co.uk>
2019-09-29 15:34:05 +0100

    Add a footer

---
+++ components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import css from "./styles.scss";
+
+const Footer = () => (
+  <div className={css.footer}>
+    <p>
+      &copy; {new Date().getFullYear()} {" "}
+      <a href="https://twitter.com/chrispatuzzo" target="_blank">Chris Patuzzo</a>
+    </p>
+  </div>
+);
+
+export default Footer;
--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -1,13 +1,16 @@
+import Footer from "../footer";
 import css from "./styles.scss";
 
 import "./reset.scss";
 
-const Layout = ({ children }) => (
+const Layout = ({ children }) => <>
   <div className={css.layout}>
     <div className={css.content}>
       {children}
     </div>
   </div>
-);
+
+  <Footer />
+</>;
 
 export default Layout;
