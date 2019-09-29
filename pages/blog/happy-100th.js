a5b8dda9d46202e31555290c691fbcf00cd02590
Chris Patuzzo <chris@patuzzo.co.uk>
2019-09-29 12:25:45 +0100

    Add the ‘Happy 100th, tuzz!’ blog post

---
+++ pages/blog/happy-100th.js
@@ -0,0 +1,95 @@
+import Layout from "../../components/layout";
+
+const Happy100th = () => (
+  <Layout>
+    <h1>Happy 100th, tuzz!</h1>
+
+    <p>
+    No, I'm not 100 years old. This blog marks my 100th personal repository on
+    GitHub. An arbitrary milestone that bears no real significance other than as
+    testament to my endless tinkering, insatiable curiosity and belief that
+    making things is perhaps the best way to really understand something.
+    </p>
+
+    <p>
+    And yet, in all that time, why haven't I blogged about it?
+    </p>
+
+    <p>
+    Those sleepness nights my brain won't switch off, exploring the twisting
+    avenues on some project. The occasions I'm frantically scribbling away,
+    working through the minutiae of an esoteric problem. Or the intense lucidity
+    and feeling of purpose that arises from programming for hours at a time.
+    </p>
+
+    <p>
+    It saddens me how much of this is lost to my own fleshy forgetfulness. A
+    failure to document and share outwardly what's going on between my ears.
+    Sure, I've written some extroadinarily long READMEs, but I'm kidding myself
+    if anyone will discover and engage with material of that formality.
+    </p>
+
+    <p>
+    Well change is happening. Our climate is dying and no longer will I allow
+    apathy to deter me from writing. I will not convince myself I cannot write
+    or that it's unimportant to me. The resistance is strong but I will work
+    through the drudgery to improve my writing ability so I can share what I'm
+    doing.
+    </p>
+
+    <p>
+    Why? Because the opportunity costs are too high.
+    </p>
+
+    <p>
+    There are so many brilliant, clever and inspiring people in this world and
+    here I am, working mostly in isolation on things I genuinely think are
+    captivating and interesting to a great deal of people. In return, I'd be
+    grateful for their wisdom, insight and opportunities to collaborate in
+    crafting something bolder and broader in scope than I can alone.
+    </p>
+
+    <p>
+    More than that, I believe in the Internet's ability to bring people
+    together with similar interests. Forging communities that transcend
+    geography and time. Communities that simply couldn't exist otherwise. I want
+    to play a part in that. I think I'd benefit from crawling out from under my
+    rock once in a while.
+    </p>
+
+    <p>
+    So here I am, muddy and regretful yet optimistic about the future.
+    </p>
+
+    <p>
+    I have a multitude of stories to tell, old and new about things I've built
+    and the many curiosities electrifying my brain. Most projects serve no
+    purpose other than as acts of creativity or to build a deeper understanding
+    of some topic. Usually circling the theme of computer science. Here are some
+    examples:
+    </p>
+
+    <ul>
+    <li>a neural network to classify words from the 'Twelve days of Christmas'</li>
+    <li>a 3D game engine (written in Rust) that runs in your browser</li>
+    <li>a puzzle-solving programming language I spent the best part of a year building</li>
+    </ul>
+
+    <p>
+    This is really just the tip of the iceberg. There's plenty of substance
+    beyond the shiny demos and visual delights. Whatsmore, I have a keen
+    interest in the act of programming itself. Writing aesthetically pleasing
+    code. Playing with various paradigms and architectures. Not that I claim to
+    know what on earth I'm doing.
+    </p>
+
+    <p>
+    A world of uncertainty awaits. I have innumerable questions and nonsensical
+    ideas to pursue. There are hard puzzles and enigmatic problems lurking out
+    of sight, conspiring to shut me down and chuckle at my foolish endeavour. I
+    cannot wait to meet you.
+    </p>
+  </Layout>
+);
+
+export default Happy100th;
