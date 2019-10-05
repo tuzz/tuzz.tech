3d5c9acedf696c47a7e885db96cbb6d12f94171c
Chris Patuzzo <chris@patuzzo.co.uk>
2019-10-05 22:25:42 +0100

    Add the bash-completion blog post

--- components/aside/Aside.jsx
+++ components/aside/Aside.jsx
@@ -1,14 +1,21 @@
 import { useState, useEffect } from "react";
 
-const Aside = ({ target, children }) => {
+const Aside = ({ target, children, moveDown }) => {
   const [style, setStyle] = useState(null);
 
   const alignAside = () => {
     const current = target.current;
+    if (!current) return;
+
     const rectangle = current.getBoundingClientRect();
     const offset = window.scrollY + rectangle.top;
 
-    setStyle({ top: offset, opacity: 1, transition: "opacity 0.3s" });
+    setStyle({
+      top: offset,
+      opacity: 1,
+      transition: "opacity 0.3s",
+      marginTop: moveDown,
+    });
   };
 
   useEffect(alignAside, [target]);
