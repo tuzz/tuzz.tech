7e93969b433be0c894c39cc8474ccb4ec1987129
Chris Patuzzo <chris@patuzzo.co.uk>
2019-10-14 14:48:38 +0100

    Improve readability on mobile

--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -12,6 +12,8 @@ const Layout = ({ children, title, navigation }) => <>
   <Analytics />
 
   <Head>
+    <link href="https://fonts.googleapis.com/css?family=Source+Code+Pro:400,400i,700,700i&display=swap" rel="stylesheet" />
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="author" content="Chris Patuzzo" />
 
