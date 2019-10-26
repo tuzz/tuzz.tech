efc0a38c7dc348d5cefd4fb4b6e0a636c9b77986
Chris Patuzzo <chris@patuzzo.co.uk>
2019-10-26 15:41:22 +0100

    Add a hook that detects print-related events

---
+++ hooks/use_print.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+
+const usePrint = (onChange) => {
+  const [isPrinting, setPrinting] = useState(false);
+
+  const handleChange = (printing) => {
+    if (printing === isPrinting) return;
+
+    setPrinting(printing);
+    onChange(printing);
+  };
+
+  useEffect(() => {
+    const beforeListener = window.addEventListener("beforeprint", () => handleChange(true));
+    const afterListener = window.addEventListener("afterprint", () => handleChange(false));
+
+    const printMedia = window.matchMedia("print");
+    const printListener = printMedia.addListener((e) => handleChange(e.matches));
+
+    return () => {
+      window.removeEventListener("beforeprint", beforeListener);
+      window.removeEventListener("afterprint", afterListener);
+
+      printMedia.removeListener(printListener);
+    };
+  });
+
+  return isPrinting;
+};
+
+export default usePrint;
