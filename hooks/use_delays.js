2f8293e8fd165032cea49f1b54e2c91e1319dbc6
Chris Patuzzo <chris@patuzzo.co.uk>
2019-10-26 15:41:03 +0100

    Extract delayed event handling into a hook

---
+++ hooks/use_delays.js
@@ -0,0 +1,11 @@
+import { useEffect } from "react";
+
+const useDelays = (callback, delays, dependencies = []) => {
+  useEffect(() => {
+    const timeouts = delays.map(d => window.setTimeout(callback, d));
+
+    return () => timeouts.forEach(t => window.clearTimeout(t));
+  }, dependencies);
+};
+
+export default useDelays;
