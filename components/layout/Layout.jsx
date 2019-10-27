1cef69f3c854da87c148e7a260312a93c5df96c5
Chris Patuzzo <chris@patuzzo.co.uk>
2019-10-27 15:51:49 +0000

    Use a static reset.css so it can be reused for examples

--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -6,13 +6,12 @@ import Footer from "../footer";
 import ExternalLinks from "../external_links";
 import css from "./styles.scss";
 
-import "./reset.scss";
-
 const Layout = ({ children, title, navigation }) => <>
   <Analytics />
 
   <Head>
     <link href="https://fonts.googleapis.com/css?family=Source+Code+Pro:400,400i,700,700i&display=swap" rel="stylesheet" />
+    <link href="/styles/reset.css" rel="stylesheet" />
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="author" content="Chris Patuzzo" />
