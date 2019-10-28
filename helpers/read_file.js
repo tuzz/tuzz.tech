a5e6902a26fa59b72b972d3e0ebd3d8112ebe1bf
Chris Patuzzo <chris@patuzzo.co.uk>
2019-10-28 10:58:51 +0000

    Make readFile check for common errors

--- helpers/read_file.js
+++ helpers/read_file.js
@@ -4,17 +4,25 @@ import findIndexes from "./find_indexes";
 const readFile = (path, marker) => {
   const content = require(`!!raw-loader!../${path}`).default;
 
-  return marker ? selectWithin(marker, content) : content;
+  return marker ? selectWithin(marker, content, path) : content;
 }
 
-const selectWithin = (marker, content) => {
+const selectWithin = (marker, content, path) => {
   const lines = content.split("\n");
   const [a, b] = findIndexes(2, lines, l => l.includes(marker));
 
-  const selection = lines.slice(a + 1, b);
-  const indented = selection.map(l => ` ${l}`);
+  if (typeof a === "undefined") {
+    throw new Error(`Could not find '${marker}' in ${path}`);
+  }
 
-  return dedent(indented.join("\n"));
+  const selection = lines.slice(a + 1, b);
+  const dedented = dedent(selection.map(l => ` ${l}`).join("\n"));
+
+  if (dedented === "") {
+    throw new Error(`Selection empty for '${marker}' in ${path}`);
+  }
+
+  return dedented;
 }
 
 export default readFile;
