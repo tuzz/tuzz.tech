2d152bc2dc89a881daba7ddcb53078fa4afba915
Chris Patuzzo <chris@patuzzo.co.uk>
2019-10-31 15:36:37 +0000

    Make headers into links

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -1,13 +1,22 @@
 import css from "./styles.scss";
 
-export const H1 = ({ children }) => (
-  <h1 className={css.h1}>{children}</h1>
+export const H1 = ({ id, children }) => (
+  <h1 id={id} className={css.h1}>{children}</h1>
 );
 
-export const H2 = ({ children }) => (
-  <h2 className={css.h2}>{children}</h2>
+export const H2 = ({ id, children }) => (
+  <a href={`#${id}`}>
+    <h2 id={throwIfMissing(id)} className={css.h2}>{children}</h2>
+  </a>
 );
 
-export const H3 = ({ children }) => (
-  <h3 className={css.h3}>{children}</h3>
+export const H3 = ({ id, children }) => (
+  <a href={`#${id}`}>
+    <h3 id={throwIfMissing(id)} className={css.h3}>{children}</h3>
+  </a>
 );
+
+const throwIfMissing = (id) => {
+  if (!id) { throw new Error("id must be set for header"); }
+  return id;
+};
