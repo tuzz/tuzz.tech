2b88afde41e931deecc439c7d7b3f85b0a5510e1
Chris Patuzzo <chris@patuzzo.co.uk>
2019-11-02 13:11:11 +0000

    Scroll to top when navigating

--- app/components/root/Root.jsx
+++ app/components/root/Root.jsx
@@ -1,10 +1,20 @@
 import React from "react"
+import { Router } from "@reach/router"
 import { Root, Routes } from "react-static"
 
 export default () => (
   <Root>
     <React.Suspense fallback="">
-      <Routes />
+      <Router>
+        <ScrollToTop default >
+          <Routes default />
+        </ScrollToTop>
+      </Router>
     </React.Suspense>
   </Root>
 );
+
+const ScrollToTop = ({ location, children }) => {
+  React.useEffect(() => window.scrollTo(0, 0), [location.pathname]);
+  return children;
+}
