e2ad5d3efaba680954408e35dd224bc5105889a5
Chris Patuzzo <chris@patuzzo.co.uk>
2019-11-02 16:37:32 +0000

    Figure out which article is next/previous inside NavBar

--- app/components/nav_bar/NavBar.jsx
+++ app/components/nav_bar/NavBar.jsx
@@ -2,8 +2,29 @@ import React from "react";
 import A from "../anchor";
 import css from "./styles.scss";
 
-const NavBar = ({ navigation }) => {
-  const { links } = navigation;
+// TODO: extract data so it can be used to build sitemap and index pages
+//  - also include titles, publish dates, description?
+//  - these can be used inside the MDX files
+
+const orderedPaths = [
+  "/blog/happy-100th",
+  "/blog/how-bash-completion-works",
+  "/blog/adding-bash-completion",
+  "/blog/palindromic-superpermutations",
+  "/blog/react-commentary-sidebar",
+  "/blog/react-commentary-sidebar-2",
+];
+
+const NavBar = ({ path }) => {
+  const index = orderedPaths.indexOf(path);
+
+  const previous = orderedPaths[index - 1];
+  const next = orderedPaths[index + 1];
+
+  const links = [
+    { text: "← Previous", href: previous },
+    { text: "Next Article →", href: next },
+  ];
 
   // Override breadcrumbs for now since we don't have index pages.
   const breadcrumbs = [
