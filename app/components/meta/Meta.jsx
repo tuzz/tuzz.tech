8afc56dca6d016d872d0546213d9a9b8fa6b9f0a
Chris Patuzzo <chris@patuzzo.co.uk>
2019-11-02 11:06:51 +0000

    Move ImageMeta and VideoMeta into Meta

--- app/components/meta/Meta.jsx
+++ app/components/meta/Meta.jsx
@@ -7,4 +7,20 @@ const Meta = (props) => (
   </Head>
 );
 
+Meta.Image = ({ path, type, width, height }) => <>
+  <Meta property="og:image" content={`https://tuzz.tech${path}`} />
+  <Meta property="og:image:secure_url" content={`https://tuzz.tech${path}`} />
+  <Meta property="og:image:type" content={`image/${type}`} />
+  <Meta property="og:image:width" content={`${width}`} />
+  <Meta property="og:image:height" content={`${height}`} />
+</>;
+
+Meta.Video = ({ path, type, width, height }) => <>
+  <Meta property="og:video" content={`https://tuzz.tech${path}`} />
+  <Meta property="og:video:secure_url" content={`https://tuzz.tech${path}`} />
+  <Meta property="og:video:type" content={`video/${type}`} />
+  <Meta property="og:video:width" content={`${width}`} />
+  <Meta property="og:video:height" content={`${height}`} />
+</>;
+
 export default Meta;
