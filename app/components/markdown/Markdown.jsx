f7623a45bb8af41dc43526cacc0d97c1ae0fa777
Chris Patuzzo <chris@patuzzo.co.uk>
2019-11-03 14:50:56 +0000

    Use MDX shortcodes for H1, H2, H3

--- app/components/markdown/Markdown.jsx
+++ app/components/markdown/Markdown.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import { MDXProvider } from "@mdx-js/react"
 
 import Title from "../title";
+import { H1, H2, H3 } from "../header";
 import Anchor from "../anchor";
 import Highlight from "../highlight";
 
 const components = {
   title: props => <Title {...props} />,
+  h1: props => <H1 {...props} />,
+  h2: props => <H2 {...props} />,
+  h3: props => <H3 {...props} />,
   a: props => <Anchor {...props} />,
   pre: props => <div {...props} />,
   code: props => <Highlight {...props} inline={false} />,
