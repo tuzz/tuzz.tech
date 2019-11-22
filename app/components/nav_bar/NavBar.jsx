78b2f296d1afd4a061314bc1277f64c5d6463e8b
Chris Patuzzo <chris@patuzzo.co.uk>
2019-11-22 15:40:28 +0000

    Pass pages into NavBar to re-use paths

--- app/components/nav_bar/NavBar.jsx
+++ app/components/nav_bar/NavBar.jsx
@@ -2,45 +2,19 @@ import React from "react";
 import A from "../anchor";
 import css from "./styles.scss";
 
-// TODO: extract data so it can be used to build sitemap and index pages
-//  - also include titles, publish dates, description?
-//  - these can be used inside the MDX files
-
-const orderedPaths = [
-  "/blog/happy-100th",
-  "/blog/how-bash-completion-works",
-  "/blog/adding-bash-completion",
-  "/blog/palindromic-superpermutations",
-  "/blog/react-commentary-sidebar",
-  "/blog/react-commentary-sidebar-2",
-  "/blog/grappling-with-infinity",
-];
-
-const NavBar = ({ path }) => {
-  const index = orderedPaths.indexOf(path);
-
-  const previous = orderedPaths[index - 1];
-  const next = orderedPaths[index + 1];
-
-  const links = [
-    { text: "← Previous", href: previous },
-    { text: "Next Article →", href: next },
-  ];
-
-  // Override breadcrumbs for now since we don't have index pages.
-  const breadcrumbs = [
-    { text: "tuzz.tech", href: "/blog/grappling-with-infinity" },
-    { text: "blog", href: "/blog/grappling-with-infinity" },
-  ];
+const NavBar = ({ previous, next }) => {
+  const latest = "/blog/grappling-with-infinity";
 
   return (
     <div className={css.nav_bar}>
       <span className={css.breadcrumbs}>
-        {breadcrumbs.map(({ text, href }, i) => <A key={i} href={href}>{text}</A>)}
+        <A href={latest}>tuzz.tech</A>
+        <A href={latest}>blog</A>
       </span>
 
       <span className={css.links}>
-        {links.map(({ text, href }, i) => <A key={i} href={href}>{text}</A>)}
+        <A href={previous && previous.path}>← Previous</A>
+        <A href={next && next.path}>Next Article →</A>
       </span>
     </div>
   );
