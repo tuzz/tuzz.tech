cab1cace5e2a9f2750164b5151f261fa635871b7
Chris Patuzzo <chris@patuzzo.co.uk>
2019-11-23 13:49:57 +0000

    Don’t import MDX files
    The MDX plugin sets the default export to the file
    content and this doesn’t play well with
    react-static which dynamically loads content as
    needed.
    
    Instead, write (another) script, this time to
    generate page_listing.js by reading the MDX files.
    It’s a bit hacky but it’ll do for now.

--- app/helpers/page_listing.js
+++
@@ -1,9 +0,0 @@
-// Order is important:
-
-export * as happy100th from "../pages/blog/happy-100th.mdx";
-export * as howBashCompletionWorks from "../pages/blog/how-bash-completion-works.mdx";
-export * as addingBashCompletion from "../pages/blog/adding-bash-completion.mdx";
-export * as palindromicSuperpermutations from "../pages/blog/palindromic-superpermutations.mdx";
-export * as reactCommentarySidebar from "../pages/blog/react-commentary-sidebar.mdx";
-export * as reactCommentarySidebar2 from "../pages/blog/react-commentary-sidebar-2.mdx";
-export * as grapplingWithInfinity from "../pages/blog/grappling-with-infinity.mdx";
