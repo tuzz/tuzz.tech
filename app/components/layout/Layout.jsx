71ca97fc0dbe6e5cfbf7021c1b47f8e45f606df8
Chris Patuzzo <chris@patuzzo.co.uk>
2019-11-23 11:01:38 +0000

    Add an empty favicon to prevent a browser request
    https://stackoverflow.com/questions/1321878/how-to-prevent-favicon-ico-requests#answer-13416784

--- app/components/layout/Layout.jsx
+++ app/components/layout/Layout.jsx
@@ -17,6 +17,7 @@ Layout.Static = ({ children }) => {
     <Head>
       <link href="https://fonts.googleapis.com/css?family=Merriweather:400,400i,700,700i|Source+Code+Pro:400,400i,700,700&display=swap" rel="stylesheet" />
       <link rel="alternate" type="application/rss+xml" title="tuzz.tech" href="/feed.xml" />
+      <link rel="icon" href="data:;base64,iVBORw0KGgo=" />
 
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta name="author" content="Chris Patuzzo" />
