55262bc01acab10548605eabdc8f3a709784352f
Chris Patuzzo <chris@patuzzo.co.uk>
2020-10-02 13:14:43 +0100

    Deploy changes

---
+++ templates/__react_static_root__/app/pages/index.32b2a92f.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{79:function(e,t,n){"use strict";n.r(t);t.default=function(){return React.createElement(Head,null,React.createElement("meta",{httpEquiv:"Refresh",content:"0; url=/blog/taking-the-plunge"}))}}}]);
